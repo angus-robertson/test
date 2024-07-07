f52ead8d00d0ad65922a7a60b1bf67041e0267d7
angus-robertson <angus.robertson@outlook.com.au>
2024-07-07 15:22:51 +1000

    update

--- .github/actions/generate-tiles.sh
+++ .github/actions/generate-tiles.sh
@@ -1,3 +1,3 @@
 mkdir -p ./client/dist/tiles
 
-tippecanoe --no-feature-limit --no-tile-size-limit --exclude-all --minimum-zoom=5 --maximum-zoom=g --output-to-directory "./client/dist/tiles" `find ./data -type f | grep .geojson`
+tippecanoe --no-feature-limit --no-tile-size-limit --no-tile-compression --exclude-all --minimum-zoom=5 --maximum-zoom=g --output-to-directory "./client/dist/tiles" `find ./data -type f | grep .geojson`
